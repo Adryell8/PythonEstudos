c43efff7f87e97a864e22f8912dd35d8d607e2d9
Lucas Adryell Ramalho <adryelll85@gmail.com>
2023-05-03 22:52:46 -0300

    Atividade média aritmética

---
+++ MediaAritmetica.py
@@ -0,0 +1,9 @@
+print('Informe 4 valores: ')
+num1 = int(input('Informe o primeiro valor: '))
+num2 = int(input('Informe o segundo valor: '))
+num3 = int(input('Informe o terceiro valor: '))
+num4 = int(input('Informe o quarto valor: '))
+
+media = (num1 + num2 + num3 + num4)/4
+
+print('A média aritmética dos valores: {}, {}, {}, {} é: {} '.format(num1, num2, num3, num4, media))
